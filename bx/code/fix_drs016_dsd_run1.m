b732e5b06555c5254c1cc11b9aebd7acea1fcdbb
wem3 <neurd@icloud.com>
2015-03-15 21:32:06 -0700

    update to run on text

--- bx/code/fix_drs016_dsd_run1.m
+++ bx/code/fix_drs016_dsd_run1.m
@@ -6,7 +6,6 @@
 % to recapture last 20 trials or so (two trials repeated)
 %
 % fortunately, a long null event preceeds the next meaningful trial
-
 % load first part of dsd_run1 output
 load('/Users/wem3/Desktop/DRS/task/hold/drs016_dsd_run1a.mat');
 A = task;
@@ -15,20 +14,14 @@ clear task;
 load('/Users/wem3/Desktop/DRS/task/hold/drs016_dsd_run1b.mat');
 B = task;
 clear task;
-
 rawCombo = A.output.raw;
 rawCombo([26:end],:) = B.output.raw(:,:);
-
 % correct timing (this is a patch to facilitate batching)
 rawCombo([26:end],7) = (rawCombo([26:end],7) + 250);
 rawCombo([26:end],10) = (rawCombo([26:end],10) + 250);
-
 B.output.raw = rawCombo;
-
 B.payout = nansum(B.output.raw(:,13))
-
 task = B;
-
 saveName = '/Users/wem3/Desktop/DRS/task/output/drs016_dsd_run1.mat';
 save(saveName,'task');
 
