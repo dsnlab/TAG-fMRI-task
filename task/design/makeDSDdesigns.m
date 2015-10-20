501e920d0a1ee6c62f4b6f24731d5c1de912cde2
John Flournoy <uoflournoy@gmail.com>
2015-10-20 11:56:49 -0700

    modifications to get correct statement types and started to modify for correct coins

--- task/design/makeDSDdesigns.m
+++ task/design/makeDSDdesigns.m
@@ -1,24 +1,19 @@
 GAdir = 'GAoutput';
 targetDirectory = '../task/input';
+NSubsTotal = 200;
+trialLength = 8;
+
 
 %% load up the 200 optimized sequences, assign 4 per 'design'
-for kCount = 1:50
+for kCount = 1:NSubsTotal
   designFile = ([GAdir,filesep,'kaoDSDdesign_', num2str(kCount), '.mat']);
   load(designFile)
-  run1.target.ovf = Out.bestOVF;
-  run1.target.sequence = Out.bestList;
-  designFile = ([GAdir,filesep,'kaoDSDdesign_', num2str(kCount + 50), '.mat']);
+  run1.ovf = Out.bestOVF;
+  run1.sequence = Out.bestList;
+  designFile = ([GAdir,filesep,'kaoDSDdesign_', num2str(kCount + NSubsTotal), '.mat']);
   load(designFile)
-  run2.target.ovf = Out.bestOVF;
-  run2.target.sequence = Out.bestList;
-  designFile = ([GAdir,filesep,'kaoDSDdesign_', num2str(kCount + 100), '.mat']);
-  load(designFile)
-  run1.coin.ovf = Out.bestOVF;
-  run1.coin.sequence = Out.bestList(Out.bestList~=0);
-  designFile = ([GAdir,filesep,'kaoDSDdesign_', num2str(kCount + 150), '.mat']);
-  load(designFile)
-  run2.coin.ovf = Out.bestOVF;
-  run2.coin.sequence = Out.bestList(Out.bestList~=0);
+  run2.ovf = Out.bestOVF;
+  run2.sequence = Out.bestList;
   dsdDesign(kCount).run1 = run1;
   dsdDesign(kCount).run2 = run2;
 end
@@ -34,7 +29,8 @@ end
 % in each condition)
 
 %!! use new frequencies
-coinPairs = [1,1; 1,2; 1,3; 1,4; 1,0; 2,1; 2,2; 2,3; 2,4; 2,0; 3,1; 3,2; 3,3; 3,4; 3,0; 4,1; 4,2; 4,3; 4,4; 4,0; 0,1; 0,2; 0,3; 0,4; 0,0; 1,1; 2,2; 3,3; 4,4; 0,0];
+coinPairs = [repmat([2,2],4,1); repmat([3,3],5,1); repmat([4,4],5,1); repmat([2,3],4,1); repmat([3,4],4,1); repmat([2,4],6,1); repmat([3,2],4,1);...
+    repmat([4,3],4,1); repmat([4,2],6,1)];
 leftIsMoreCount = 1;
 rightIsMoreCount = 1;
 eqCount = 1;
@@ -57,10 +53,17 @@ end
 % add statements to designs at random
 %!! Distinguish between types of statements to correspond with optimized
 %!! ordering of neutral and affective.
-statementFile = 'materials/statements.txt';
-rawStatements(:,1) = textread(statementFile,'%s','delimiter','\n');
-numTrials = length(rawStatements)/2;
-rawStatements = Shuffle(rawStatements);
+neutStatementFile = 'materials/statements_neutral.txt';
+affStatementFile = 'materials/statements_affect.txt';
+
+neutRawStatements(:,1) = textread(neutStatementFile,'%s','delimiter','\n');
+affRawStatements(:,1) = textread(affStatementFile,'%s','delimiter','\n');
+
+numTrials = (length(neutRawStatements)+length(affRawStatements))/2;
+
+neutStatements = shuffle(neutRawStatements);
+affStatements = shuffle(affRawStatements);
+
 %% convert the 0s in the sequence into + 8sec durations for previous trial,
 % get jittered durations (m = 1.5s)
 %!! need to make sure these rests due to optimization are not negatively
@@ -68,21 +71,21 @@ rawStatements = Shuffle(rawStatements);
 rawJitter = (0.5:.0225:1.5)';
 choiceJitter = (0.3:0.009:0.7)';
 
-for dCount = 1:50
-    rawStatements=Shuffle(rawStatements);
+for dCount = 1:NSubsTotal
+    rawStatements=shuffle(rawStatements);
     for rCount = 1:2;
         thisRun = (['run',num2str(rCount)]);
-        dsdDesign(dCount).(thisRun).statement = rawStatements( (1:45)+ 45*(rCount-1)  ); % take the first 
-        rawJitter = Shuffle(rawJitter);
-        choiceJitter = Shuffle(choiceJitter);
-        adjJitter = NaN(45,1);
-        condition = NaN(45,1);
+        %dsdDesign(dCount).(thisRun).statement = rawStatements( (1:numTrials)+ numTrials*(rCount-1)  ); % take the first 
+        rawJitter = shuffle(rawJitter);
+        choiceJitter = shuffle(choiceJitter);
+        adjJitter = NaN(numTrials,1);
+        condition = NaN(numTrials,1);
         zCount = 0;
-        rawTarget = dsdDesign(dCount).(thisRun).target.sequence; % Every event from optimized sequence (e.g., 1 2 0 3 3 2 ...)
+        rawTarget = dsdDesign(dCount).(thisRun).sequence; % Every event from optimized sequence (e.g., numbers 0-6 ...)
         for eCount = 1:length(rawTarget) % for every event
             if rawTarget(eCount) == 0 % if it's a rest event
                 zCount = zCount + 1; % add one to the counter variable for number of rests
-                adjJitter(eCount - zCount) = (rawJitter(eCount - zCount) + 8); % this way of indexing ensures it adds 8 seconds to the previous trial
+                adjJitter(eCount - zCount) = (rawJitter(eCount - zCount) + trialLength); % this way of indexing ensures it adds 8 seconds to the previous trial
             else
                 adjJitter(eCount - zCount) = rawJitter(eCount - zCount);
             end
@@ -94,74 +97,67 @@ for dCount = 1:50
         % some temporary NaN vectors
         %!! We can get rid of the below loop because we only care about
         %share versus private (we don't have parents)
-        targetA = nan(45,1);
-        targetB = nan(45,1);
-        leftoRighto = rand(45,1);
+
+        leftoRighto = rand(numTrials,1);
+        
+        nStatementCounter = 1;
+        aStatementCounter = 1;
+        % Fill design list with affective or neutral statements depending on
+        % condition.
         for tCount = 1:length(condition)
-            if condition(tCount) == 1;
-                targetA(tCount) = 1;
-                targetB(tCount) = 2;
-            elseif condition(tCount) == 2;
-                targetA(tCount) = 1;
-                targetB(tCount) = 3;
-            elseif condition(tCount) == 3;
-                targetA(tCount) = 2;
-                targetB(tCount) = 3;
+            if condition(tCount) > 0 & condition(tCount) < 4;
+                dsdDesign(dCount).(thisRun).statement(tCount) = neutStatements(nStatementCounter + numTrials*(rCount-1));
+                nStatementCounter = nStatementCounter + 1;
+            elseif condition(tCount) > 3 & condition(tCount) < 7;
+                dsdDesign(dCount).(thisRun).statement(tCount) = affStatements(aStatementCounter + numTrials*(rCount-1));
+                aStatementCounter = aStatementCounter + 1;
             else
                 error('What the fucking fuck?!')
             end
         end
 
-        coinA = nan(45,1);
-        coinB = nan(45,1);
-        Apairs = leftIsMorePairs(Shuffle(1:10),:);
-        Bpairs = rightIsMorePairs(Shuffle(1:10),:);
-        Epairs = eqPairs(Shuffle(1:10),:);
+        coinA = nan(numTrials,1);
+        coinB = nan(numTrials,1);
+        Apairs = leftIsMorePairs(shuffle(1:14),:);
+        Bpairs = rightIsMorePairs(shuffle(1:14),:);
+        Epairs = eqPairs(shuffle(1:14),:);
         % get a different randomized order of coin pairs for each subCondition
         %!! Edit because we don't have t1-3 conditions (but we have affect
         %and neutral
-        run1t1c1 = Apairs((1:5), :);
-        run2t1c1 = Apairs((6:10),:);
-        Apairs = leftIsMorePairs(Shuffle(1:10),:);
-        Bpairs = rightIsMorePairs(Shuffle(1:10),:);
-        Epairs = eqPairs(Shuffle(1:10),:);
-        run1t1c2 = Bpairs((1:5), :);
-        run2t1c2 = Bpairs((6:10),:);
-        Apairs = leftIsMorePairs(Shuffle(1:10),:);
-        Bpairs = rightIsMorePairs(Shuffle(1:10),:);
-        Epairs = eqPairs(Shuffle(1:10),:);
-        run1t1c3 = Epairs((1:5), :);
-        run2t1c3 = Epairs((6:10),:);
-        Apairs = leftIsMorePairs(Shuffle(1:10),:);
-        Bpairs = rightIsMorePairs(Shuffle(1:10),:);
-        Epairs = eqPairs(Shuffle(1:10),:);
-        run1t2c1 = Apairs((1:5), :);
-        run2t2c1 = Apairs((6:10),:);
-        Apairs = leftIsMorePairs(Shuffle(1:10),:);
-        Bpairs = rightIsMorePairs(Shuffle(1:10),:);
-        Epairs = eqPairs(Shuffle(1:10),:);
-        run1t2c2 = Bpairs((1:5), :);
-        run2t2c2 = Bpairs((6:10),:);
-        Apairs = leftIsMorePairs(Shuffle(1:10),:);
-        Bpairs = rightIsMorePairs(Shuffle(1:10),:);
-        Epairs = eqPairs(Shuffle(1:10),:);
-        run1t2c3 = Epairs((1:5), :);
-        run2t2c3 = Epairs((6:10),:);
-        Apairs = leftIsMorePairs(Shuffle(1:10),:);
-        Bpairs = rightIsMorePairs(Shuffle(1:10),:);
-        Epairs = eqPairs(Shuffle(1:10),:);
-        run1t3c1 = Apairs((1:5), :);
-        run2t3c1 = Apairs((6:10),:);
-        Apairs = leftIsMorePairs(Shuffle(1:10),:);
-        Bpairs = rightIsMorePairs(Shuffle(1:10),:);
-        Epairs = eqPairs(Shuffle(1:10),:);
-        run1t3c2 = Bpairs((1:5), :);
-        run2t3c2 = Bpairs((6:10),:);
-        Apairs = leftIsMorePairs(Shuffle(1:10),:);
-        Bpairs = rightIsMorePairs(Shuffle(1:10),:);
-        Epairs = eqPairs(Shuffle(1:10),:);
-        run1t3c3 = Epairs((1:5), :);
-        run2t3c3 = Epairs((6:10),:);
+        % Left is Private, Right is Share
+        % t1 = Neutral
+        % t2 = Affective
+        % c1 = loss to share
+        % c2 = loss to private
+        % c3 = equal payout
+        run1t1c1 = Apairs((1:7), :);
+        run2t1c1 = Apairs((8:14),:);
+        Apairs = leftIsMorePairs(shuffle(1:14),:);
+        Bpairs = rightIsMorePairs(shuffle(1:14),:);
+        Epairs = eqPairs(shuffle(1:14),:);
+        run1t1c2 = Bpairs((1:7), :);
+        run2t1c2 = Bpairs((8:14),:);
+        Apairs = leftIsMorePairs(shuffle(1:14),:);
+        Bpairs = rightIsMorePairs(shuffle(1:14),:);
+        Epairs = eqPairs(shuffle(1:14),:);
+        run1t1c3 = Epairs((1:7), :);
+        run2t1c3 = Epairs((8:14),:);
+        Apairs = leftIsMorePairs(shuffle(1:14),:);
+        Bpairs = rightIsMorePairs(shuffle(1:14),:);
+        Epairs = eqPairs(shuffle(1:14),:);
+        run1t2c1 = Apairs((1:7), :);
+        run2t2c1 = Apairs((8:14),:);
+        Apairs = leftIsMorePairs(shuffle(1:14),:);
+        Bpairs = rightIsMorePairs(shuffle(1:14),:);
+        Epairs = eqPairs(shuffle(1:14),:);
+        run1t2c2 = Bpairs((1:7), :);
+        run2t2c2 = Bpairs((8:14),:);
+        Apairs = leftIsMorePairs(shuffle(1:14),:);
+        Bpairs = rightIsMorePairs(shuffle(1:14),:);
+        Epairs = eqPairs(shuffle(1:14),:);
+        run1t2c3 = Epairs((1:7), :);
+        run2t2c3 = Epairs((8:14),:);
+        
 
         t1c1Count = 1;
         t1c2Count = 1;
@@ -169,15 +165,10 @@ for dCount = 1:50
         t2c1Count = 1;
         t2c2Count = 1;
         t2c3Count= 1;
-        t3c1Count = 1;
-        t3c2Count = 1;
-        t3c3Count= 1;
 
-
-        for tCount = 1:45 % (there are 45 trials, screw generalizing this)
+        for tCount = 1:numTrials % (there are numTrials trials, screw generalizing this)
             switch condition(tCount)
-                case 1
-                    switch dsdDesign(dCount).(thisRun).coin.sequence(tCount)
+               
                         case 1
                             coinA(tCount) = run1t1c1(1,1);
                             coinB(tCount) = run1t1c1(1,2);
@@ -191,39 +182,22 @@ for dCount = 1:50
                             coinA(tCount) = run1t1c3(1,1);
                             coinB(tCount) = run1t1c3(1,2);
                             run1t1c3 = popArray(run1t1c3);
-                    end
+    
 
-                case 2
-                    switch dsdDesign(dCount).(thisRun).coin.sequence(tCount)
-                        case 1
+                
+                        case 4
                             coinA(tCount) = run1t2c1(1,1);
                             coinB(tCount) = run1t2c1(1,2);
                             run1t2c1 = popArray(run1t2c1);
-                        case 2
+                        case 5
                             coinA(tCount) = run1t2c2(1,1);
                             coinB(tCount) = run1t2c2(1,2);
                             run1t2c2 = popArray(run1t2c2);
-                        case 3
+                        case 6
                             coinA(tCount) = run1t2c3(1,1);
                             coinB(tCount) = run1t2c3(1,2);
                             run1t2c3 = popArray(run1t2c3);
-                    end
 
-                case 3
-                    switch dsdDesign(dCount).(thisRun).coin.sequence(tCount)
-                        case 1
-                            coinA(tCount) = run1t3c1(1,1);
-                            coinB(tCount) = run1t3c1(1,2);
-                            run1t3c1 = popArray(run1t3c1);
-                        case 2
-                            coinA(tCount) = run1t3c2(1,1);
-                            coinB(tCount) = run1t3c2(1,2);
-                            run1t3c2 = popArray(run1t3c2);
-                        case 3
-                            coinA(tCount) = run1t3c3(1,1);
-                            coinB(tCount) = run1t3c3(1,2);
-                            run1t3c3 = popArray(run1t3c3);
-                    end
             end
 
             % randomly determine screen position
@@ -252,7 +226,7 @@ end
 
 % because having double rests at the end makes the loop funky, manually fix
 % sub007.run2:
-dsdDesign(7).run2.discoJitter(45) = dsdDesign(7).run2.discoJitter(45) + 8;
+dsdDesign(7).run2.discoJitter(numTrials) = dsdDesign(7).run2.discoJitter(numTrials) + 8;
 %save dsdDesigns.mat dsdDesign
 
 caBut('dsdDesign','targetDirectory'); %!! is problem?
@@ -275,7 +249,7 @@ for dCount = 1:50
         choiceJitter = dsdDesign(dCount).(thisRun).choiceJitter;
         discoJitter = dsdDesign(dCount).(thisRun).discoJitter;
 
-        for tCount = 1:45
+        for tCount = 1:numTrials
           fid = fopen([targetDirectory,filesep,subID,'_dsd_','run',num2str(rCount),'_input.txt'],'a');
 
           fprintf(fid,'%u,%u,%u,%u,%u,%u,%4.3f,%4.3f,%s\n',tCount,condition(tCount),leftTarget(tCount),rightTarget(tCount),leftCoin(tCount),rightCoin(tCount),choiceJitter(tCount),discoJitter(tCount),statement{tCount});
