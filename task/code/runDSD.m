5c133443e89982f0f373d64cf58a0c51f620d772
Marge <marge@dyn-184-171-77-159.uoregon.edu>
2016-01-07 12:09:02 -0800

    added info about output

--- task/code/runDSD.m
+++ task/code/runDSD.m
@@ -10,10 +10,12 @@ function [task] = runDSD(subNumArg, runNumArg)
 %    (subID)_dsd_(runNum).txt = comma delimited text with per trial input
 %    input text columns (%u,%u,%u,%u,%u,%f%f) 
 %       1. trialNum
-%       2. condition (self vs. friend == 1, self vs. parent == 2, friend vs. parent == 3)
-%       3. leftTarget (self == 1, friend == 2, parent == 3) 
+%       2. condition
+%           [1-3] = Neutral; [4-6] = Affective;
+%           [1,4] = loss to share; [2, 5] = loss to private; [3,6] equal
+%       3. leftTarget (self == 1, friend == 2) 
 %       4. rightTarget
-%       5. leftCoin (1:4 == 1:4 coins, 5 == 0 coins, for indexing convenience)
+%       5. leftCoin
 %       6. rightCoin
 %       7. choiceJitter (%f, amount of time to vary gap b/w choice & disclosure)
 %       8. discoJitter (%f, amount of time to vary ITI)
@@ -22,6 +24,26 @@ function [task] = runDSD(subNumArg, runNumArg)
 %     DRSstim.mat = structure w/ precompiled image matrices (coins, hands, etc.)
 %
 %     (subID)_info.mat = structure w/ subject specific info
+% Output:
+%   A stucture named 'task' within which these may be of interest
+%
+%   task.output.raw:
+%       1. Trial Number
+%       2. condition
+%           [1-3] = Neutral; [4-6] = Affective;
+%           [1,4] = loss to share; [2, 5] = loss to private; [3,6] equal
+%       3. leftTarget (self == 1, friend == 2) 
+%       4. rightTarget
+%       5. leftCoin
+%       6. rightCoin
+%       7. Time since trigger for disclosure (choiceOnset - loopStartTime);
+%       8. choiceResponse - Share or not? (leftkeys = 1, rightkeys = 2)
+%       9. choiceRT - reaction time
+%       10. Time since trigger for statement decisions (discoOnset - loopStartTime);
+%       11. discoResponse - enorse or not?  (leftkeys = 1, rightkeys = 2)
+%       12. discoRT - reaction time
+%   task.input.statement
+%   task.payout
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 switch nargin
